5d2f3527e9a0352fc21e55fe3a3bf01acc161332
Alwin Sabu <alwinsabu2028@cs.sjcetpalai.ac.in>
2025-09-09 14:50:05 +0530

    Update Sparse_Matrix.c

--- Sparse_Matrix.c
+++ Sparse_Matrix.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 
 int main() {
     int row1, col1, row2, col2;
@@ -16,13 +16,11 @@ int main() {
     printf("Enter the column size of the second matrix: ");
     scanf("%d", &col2);
 
-
     if (row1 != row2 || col1 != col2) {
         printf("Matrix dimensions do not match. Cannot perform addition.\n");
         return 0;
     }
 
-    
     printf("Enter the elements of the first matrix:\n");
     for (i = 0; i < row1; i++) {
         for (j = 0; j < col1; j++) {
@@ -31,7 +29,6 @@ int main() {
         }
     }
 
- 
     printf("Enter the elements of the second matrix:\n");
     for (i = 0; i < row2; i++) {
         for (j = 0; j < col2; j++) {
@@ -40,7 +37,7 @@ int main() {
         }
     }
 
-  
+    // Convert A to sparse matrix
     for (i = 0; i < row1; i++) {
         for (j = 0; j < col1; j++) {
             if (A[i][j] != 0) {
@@ -55,6 +52,7 @@ int main() {
     sp_mat1[0][1] = col1;
     sp_mat1[0][2] = k - 1;
 
+    // Convert B to sparse matrix
     for (i = 0; i < row2; i++) {
         for (j = 0; j < col2; j++) {
             if (B[i][j] != 0) {
@@ -69,7 +67,7 @@ int main() {
     sp_mat2[0][1] = col2;
     sp_mat2[0][2] = u - 1;
 
-   
+    // Add two sparse matrices
     int p = 1, q = 1;
     while (p <= sp_mat1[0][2] && q <= sp_mat2[0][2]) {
         if (sp_mat1[p][0] < sp_mat2[q][0] ||
@@ -96,7 +94,6 @@ int main() {
         }
     }
 
-    
     while (p <= sp_mat1[0][2]) {
         sp_sum[h][0] = sp_mat1[p][0];
         sp_sum[h][1] = sp_mat1[p][1];
@@ -110,16 +107,44 @@ int main() {
         h++; q++;
     }
 
-   
     sp_sum[0][0] = row1;
     sp_sum[0][1] = col1;
     sp_sum[0][2] = h - 1;
 
-    
-    printf("\nResultant Sparse Matrix2:\n");
+    // Print sum sparse matrix
+    printf("\nResultant Sparse Matrix (Sum):\n");
     printf("Row\tCol\tVal\n");
     for (i = 0; i < h; i++) {
         printf("%d\t%d\t%d\n", sp_sum[i][0], sp_sum[i][1], sp_sum[i][2]);
+    }
+
+    // Transpose of the sum sparse matrix (inline, no function)
+    int sp_transpose[100][3];
+    int rows = sp_sum[0][0];
+    int cols = sp_sum[0][1];
+    int terms = sp_sum[0][2];
+
+    sp_transpose[0][0] = cols;  // Transpose rows = original cols
+    sp_transpose[0][1] = rows;  // Transpose cols = original rows
+    sp_transpose[0][2] = terms;
+
+    int currentIndex = 1;
+    for (int col = 0; col < cols; col++) {
+        for (int i = 1; i <= terms; i++) {
+            if (sp_sum[i][1] == col) {
+                sp_transpose[currentIndex][0] = sp_sum[i][1];  // new row = old col
+                sp_transpose[currentIndex][1] = sp_sum[i][0];  // new col = old row
+                sp_transpose[currentIndex][2] = sp_sum[i][2];
+                currentIndex++;
+            }
+        }
+    }
+
+    // Print transposed sparse matrix
+    printf("\nTransposed Sparse Matrix:\n");
+    printf("Row\tCol\tVal\n");
+    for (i = 0; i <= sp_transpose[0][2]; i++) {
+        printf("%d\t%d\t%d\n", sp_transpose[i][0], sp_transpose[i][1], sp_transpose[i][2]);
     }
 
     return 0;
